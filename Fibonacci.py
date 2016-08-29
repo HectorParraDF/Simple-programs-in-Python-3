e3c0dea623e0868838eaa690fd65fc9cc1129c0b
Hector Parra <hpdf.phys@gmail.com>
2016-08-29 19:31:10 -0400

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,18 +1,23 @@
+#This simple script prints out the Fibonacci sequence up to the entry n = rg.
+
+#Print formatting style
+style = "{0:10}{1:10}"
+
 #How many elements to display
 rg = 30;
 
 #Starting values
-i_1 = 1;
-i_2 = 1;
+i_1 = 1
+i_2 = 1
 
 #Print the starting values
-print(i_1)
-print(i_2)
+print(style.format(1, i_1))
+print(style.format(2, i_2))
 
 #Print the other rg - 2 values
-for a in range(0,rg - 2):
+for a in range(3, rg + 1):
     i_n = i_2 + i_1
-    print(i_n)
-    i_1 = i_n
-    i_2 = i_1
+    print(style.format(a, i_n))
+    i_1 = i_2
+    i_2 = i_n
 (a)
